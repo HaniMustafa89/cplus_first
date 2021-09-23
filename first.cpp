edaae9ca63c6b0a88c17aabb1ab13a4bc14dda38
HaniMustafa89 <91127297+HaniMustafa89@users.noreply.github.com>
2021-09-23 11:39:25 -0700

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+	int a,b;
+	cout<<"enter first number ";
+	cin>>a;
+	cout<<"enter second number ";
+	cin>>b;
+	cout<<"multiplication of two numbers are "<<a*b;
+}
